4917c821e2fca64aa2a20fba2bd1d7d54982fc1f
LeThiMyLan19it6 <70904020+LeThiMyLan19it6@users.noreply.github.com>
2021-11-12 18:38:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function openStream(){
+    const config = { audio: false, video: true}
+    return navigator.mediaDevices.getUserMedia(config);
+}
+function playStream(idVideoTag, stream){
+    const video = document.getElementById(idVideoTag);
+    video.srcObject = stream;
+    video.play();
+
+}
+//openStream()
+//.then(stream => playStream('localStream', stream));
+const peer = new Peer({key: 'tkv5g2acaree9udi'});
+peer.on('open', id => console.log(id));
